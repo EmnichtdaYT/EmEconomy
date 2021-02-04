e36a6addba47c2a236aa58a04239453ad0436483
EmnichtdaYT | Erik <emnichtda@gmail.com>
2021-02-04 12:39:54 +0100

    Added Holder UUID

--- src/main/java/de/dedad/emeconomy/account/Account.java
+++ src/main/java/de/dedad/emeconomy/account/Account.java
@@ -3,8 +3,14 @@ package de.dedad.emeconomy.account;
 import java.math.BigInteger;
 
 public abstract class Account {
+    private UUID holderUuid;
+    
     public abstract BigInteger getAmount();
     public abstract void setAmount(BigInteger value);
     public abstract void addAmount(BigInteger value);
     public abstract void subtractAmount(BigInteger value);
+    
+    public void getHolder(){
+        return hoderUuid;
+    }
 }
