819be0707273e09554e71d923f369219473a148f
EmnichtdaYT | Erik <emnichtda@gmail.com>
2021-02-04 12:41:42 +0100

    added constructor

--- src/main/java/de/dedad/emeconomy/account/Account.java
+++ src/main/java/de/dedad/emeconomy/account/Account.java
@@ -6,6 +6,11 @@ public abstract class Account {
     private UUID holderUuid;
     private EmEconomyMain pl;
     
+    public Account(UUID holderUuid, EmEconomyMain pl){
+        this.hoderUuid = holderUuid;
+        this.pl = pl;
+    }
+    
     public abstract BigInteger getAmount();
     public abstract void setAmount(BigInteger value);
     public abstract void addAmount(BigInteger value);
